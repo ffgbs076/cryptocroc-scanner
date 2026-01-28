f236115f9a8642b9e0e7163b286728f492cc0c13
ffgbs076 <franselsimmons@hotmail.com>
2026-01-28 16:27:47 +0100

    Delete eslint.config.mjs

--- eslint.config.mjs
+++
@@ -1,9 +0,0 @@
-import { FlatCompat } from "@eslint/eslintrc";
-
-const compat = new FlatCompat({
-  baseDirectory: process.cwd()
-});
-
-export default [
-  ...compat.extends("next/core-web-vitals")
-];
